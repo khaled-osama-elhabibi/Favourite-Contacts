f2ba5fde20e723b2cbb4e679a0d2bdce6c669199
khaledOsama <k.osama@uniparticle.com>
2022-02-09 14:27:06 +0200

    ADD : load all contacts to app and solve permissions issues

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,9 +1,52 @@
-import {Text, View, StyleSheet} from 'react-native';
-import React from 'react';
+import {Text, View, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
+import React, {useState} from 'react';
+import Contacts from 'react-native-contacts';
+import {request, PERMISSIONS} from 'react-native-permissions';
+
 const Home = () => {
+  const [contacts, setContacts] = useState();
+  const requestPermissionForAccesssContact = async () => {
+    let responseOfPermissionRequest =
+      Platform.OS === 'ios'
+        ? await request(PERMISSIONS.IOS.CONTACTS)
+        : await request(PERMISSIONS.ANDROID.READ_CONTACTS);
+
+    if (responseOfPermissionRequest === 'denied') {
+      console.log('denied');
+    } else if (responseOfPermissionRequest == 'granted') {
+      Contacts.getAll()
+        .then(contacts => {
+          // work with contacts
+          console.log(contacts);
+          setContacts(contacts);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  };
+
   return (
     <View style={styles.screen}>
-      <Text>hello</Text>
+      <TouchableOpacity
+        onPress={() => {
+          requestPermissionForAccesssContact();
+          console.log('test');
+        }}>
+        <Text style={styles.btn__text}>ACCESS Phone Contact</Text>
+      </TouchableOpacity>
+
+      <View>
+        <FlatList
+          data={contacts}
+          renderItem={({item}) => (
+            <View style={{backgroundColor: 'red', borderWidth: 1}}>
+              <Text style={styles.btn__text}>{item.displayName}</Text>
+            </View>
+          )}
+          keyExtractor={contact => contact.displayName}
+        />
+      </View>
     </View>
   );
 };
@@ -12,6 +55,9 @@ const styles = StyleSheet.create({
   screen: {
     height: '100%',
     backgroundColor: 'black',
+  },
+  btn__text: {
+    color: '#FFF',
   },
 });
 
